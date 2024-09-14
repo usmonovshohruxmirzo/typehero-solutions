6016c642125fd3f20e89e1c4499d656b22b5a61d
Shohruxmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2024-09-14 21:57:53 +0500

    feat(03): updated

--- src/03. Generic Function Arguments.ts
+++ src/03. Generic Function Arguments.ts
@@ -1,3 +1,9 @@
 const identity: <T>(n: T) => T = (n) => n;
+
 const mapArray: <T, U>(arr: T[], fn: (str: T) => U) => U[] = (arr, fn) =>
   arr.map(fn);
+
+const identityResult = identity<number>(5);
+
+const numbers = [1, 2, 3, 4, 5];
+const doubled = mapArray<number, number>(numbers, (n) => n * 2);
