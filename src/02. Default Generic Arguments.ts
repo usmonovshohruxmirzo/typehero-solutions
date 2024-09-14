2e2ec244224be90a9b91ebce28d00fc685d1ef20
Shohruxmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2024-09-14 21:45:57 +0500

    feat(02): updated

--- src/02. Default Generic Arguments.ts
+++ src/02. Default Generic Arguments.ts
@@ -1,5 +1,11 @@
-type ApiRequest<a, m = "GET"> = {
+type ApiRequest<a = 200, m = "GET"> = {
   data: a;
   method: m;
 };
+
 type TSConfig<o = { strict: true }> = o;
+
+const postRequest: ApiRequest<number, "POST"> = {
+  data: 123,
+  method: "POST",
+};
