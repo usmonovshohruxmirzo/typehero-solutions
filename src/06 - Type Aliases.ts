c99c583c0252b335fb590b5b90709c4d848c37f7
Shohruxmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2024-09-15 14:05:06 +0500

    feat: 06 - Type Aliases - SOLVED

---
+++ src/06 - Type Aliases.ts
@@ -0,0 +1,14 @@
+type Name = string;
+
+type Year = number;
+
+type Count = number;
+
+type IsOperational = boolean;
+
+type Kilograms = number;
+
+type Payload = {
+  name: Name;
+  mass: Kilograms;
+};
