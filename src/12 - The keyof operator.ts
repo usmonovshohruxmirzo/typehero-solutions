b40a890c6aeed21d0ecc9ff1d2bca9d1a2a22817
Shohruxmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2024-09-15 16:53:00 +0500

    feat: 12 - The keyof operator - SOLVED

---
+++ src/12 - The keyof operator.ts
@@ -0,0 +1,34 @@
+// solution
+type Artist = keyof typeof casettesByArtist;
+
+//
+const casettesByArtist = {
+  "Alanis Morissette": 2,
+  "Mariah Carey": 8,
+  Nirvana: 3,
+  Oasis: 2,
+  Radiohead: 3,
+  "No Doubt": 3,
+  "Backstreet Boys": 3,
+  "Spice Girls": 2,
+  "Green Day": 2,
+  "Pearl Jam": 5,
+  Metallica: 5,
+  "Guns N' Roses": 2,
+  U2: 3,
+  Aerosmith: 4,
+  "R.E.M.": 4,
+  Blur: 3,
+  "The Smashing Pumpkins": 5,
+  "Britney Spears": 3,
+  "Whitney Houston": 3,
+};
+
+const getCasetteCount = (artist: Artist) => {
+  return casettesByArtist[artist];
+};
+
+getCasetteCount("Mariah Carey");
+
+// @ts-expect-error
+getCasetteCount("Red Hot Chili Peppers");
