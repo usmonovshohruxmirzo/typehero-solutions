4d43107f07a0d13975801cd29717e60120214720
Shohruxmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2024-09-18 14:54:43 +0500

    feat: 05 Exclude - UPDATED

--- src/easy/05 Exclude.ts
+++ src/easy/05 Exclude.ts
@@ -1,5 +1,6 @@
 // solution
-type MyExclude<T, U> = Exclude<T, U>;
+// type MyExclude<T, U> = Exclude<T, U>; // 1
+type MyExclude<T, U> = T extends U ? never : T; // 2
 
 // usage
 type Result = MyExclude<"a" | "b" | "c", "a">;
